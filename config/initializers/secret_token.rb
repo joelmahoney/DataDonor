b365e0ad1daaad9dc77d558ed17d290e68db9c94
Joel Mahoney <joelmahoney@gmail.com>
2012-03-12 18:13:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datadonor::Application.config.secret_token = 'e0fd43c7731f48ed116a224e376147cda7b52cd39137f18435d0835f105ebe73d2f56533cb4d3a837af5800d6462bf028d15003286e518ebc8080b64f4da0bfe'
