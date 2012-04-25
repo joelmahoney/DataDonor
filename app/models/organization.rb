62e286c14d0b436f8455d6fc65411341e8f6cd38
Joel Mahoney <joelmahoney@gmail.com>
2012-04-24 18:42:55 -0700

    Associate projects and organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
 class Organization < ActiveRecord::Base
+  has_many :projects
+  
   attr_accessible :description, :name
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
+  belongs_to :organization
+  
   attr_accessible :description, :location, :name, :organization_id, :start_date
 end
