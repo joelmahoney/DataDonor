0a05cb78d73ce0261ce6a1e766a820420e87f47d
Joel Mahoney <joelmahoney@gmail.com>
2012-05-02 01:26:58 -0700

    Fix IP address

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -6,10 +6,10 @@ class SurveysController < ApplicationController
   
   def create
     if params[:support] == 'true'
-      Survey.create(:support => true, :ip => request.try(:remote_addr))
+      Survey.create(:support => true, :ip => request.remote_ip)
       redirect_to new_user_registration_url, :notice => 'Thanks for your interest! &nbsp;Please create an account so we can keep you up-to-date on DataDonor news and events.'
     else
-      Survey.create(:support => false, :ip => request.try(:remote_addr))
+      Survey.create(:support => false, :ip => request.remote_ip)
       redirect_to root_url, :notice => "OK. &nbsp;Thanks for the feedback."
     end
   end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,3 @@
 class Survey < ActiveRecord::Base
-  attr_accessible :support
+  attr_accessible :support, :ip
 end
